cefae930d61c36a4a9d203a92e7676f622e1dc92
Jermi Ethermias <ethermias@Jermis-MacBook-Pro.local>
2024-03-31 16:25:30 -0400

    fixes

--- backend/app/api/routes/changelog.py
+++ backend/app/api/routes/changelog.py
@@ -5,7 +5,9 @@ router = APIRouter()
 async def changelog():
   
     return {
-        "v0.1.0": "set up static site, adding dns ethermias.com",
-        "v0.1.1": "set up dynamic site, setup kubernates claster, major upgrade UI"
+        "v0.1.0": "init,
+        "v0.1.1": "set up static site, adding dns ethermias.com, Mar 23, 2024",
+        "v0.1.2": "set up dynamic site, setup kubernates claster, major upgrade UI, Mar 29, 2024"
+        "v0.1.3": "Score update, Apr 05, 2024"
     }
     
--- backend/app/api/routes/lineup.py
+++ backend/app/api/routes/lineup.py
@@ -5,10 +5,19 @@ from typing import List
 class Lineup(BaseModel):
     squad: List[conint(ge=0)]
     tag: str = None
+    submission_time: str = None
 
 router = APIRouter()
 
 @router.post("/")
 async def create_lineup(lineup: Lineup):
-    
+    current_time = datetime.now().isoformat()
+
+    lineup.submission_time = current_time
+    lineup_json = lineup.json()
+
+    # Write JSON string to file
+    with open("tags.json", "a") as f:
+        f.write(lineup_json + "\n")
+
     return { "lineup": lineup }
