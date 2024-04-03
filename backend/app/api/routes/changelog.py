e8b7391ad9aa3d64864f28cd8c47adea682ba664
Jermi Ethermias <ethermias@Jermis-MacBook-Pro.local>
2024-04-03 08:04:25 -0400

    fixes

--- backend/app/api/routes/changelog.py
+++ backend/app/api/routes/changelog.py
@@ -5,9 +5,9 @@ router = APIRouter()
 async def changelog():
   
     return {
-        "v0.1.0": "init,
+        "v0.1.0": "init",
         "v0.1.1": "set up static site, adding dns ethermias.com, Mar 23, 2024",
-        "v0.1.2": "set up dynamic site, setup kubernates claster, major upgrade UI, Mar 29, 2024"
+        "v0.1.2": "set up dynamic site, setup kubernates claster, major upgrade UI, Mar 29, 2024",
         "v0.1.3": "Score update, Apr 05, 2024"
     }
     
