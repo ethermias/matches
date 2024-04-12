ae49a30fba4b7c0c6595e786e185427de0279044
Jermi Ethermias <ethermias@Jermis-MacBook-Pro.local>
2024-04-12 08:21:25 -0400

    fixes

--- backend/app/api/server.py
+++ backend/app/api/server.py
@@ -13,6 +13,7 @@ def get_application():
         allow_origins=[    
             "http://localhost",
             "http://localhost:8000",
+            "http://127.0.0.1:8000",
             "http://localhost:3000"
         ],
         allow_credentials=True,
