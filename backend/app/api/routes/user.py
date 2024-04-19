71b155999a8f89c6e8908cf8dff869293e79fedf
Jermi Ethermias <ethermias@Jermis-MacBook-Pro.local>
2024-04-19 09:06:52 -0400

    fixes

--- backend/app/api/routes/user.py
+++ backend/app/api/routes/user.py
@@ -17,6 +17,12 @@ def log_user(logUser: LogUser):
     with open("user_tags_log.json", "a") as f:
         f.write(logUser.json() + "\n")
 
+def get_matching_value(user, data):
+    for d in data:
+        if d['email'] == user.email or d['userName'] == user.userName:
+            return d
+    return None
+
 @router.post("/signin")
 async def sign_in_user(user: User):
     filename = "user_tags.json"
@@ -25,9 +31,9 @@ async def sign_in_user(user: User):
             data = json.load(file)
     except FileNotFoundError:
         data = []
-    exists = any((d['email'] == user.email or d['userName'] == user.userName) for d in data)
-    log_user(LogUser(user=user, exists=exists))
-    return { "user": user, "isValid": exists }
+    exists = get_matching_value(user, data)
+    log_user(LogUser(user=user, exists=exists != None))
+    return {"user": exists if exists is not None else user, "isValid": exists != None}
 
 @router.post("/signup")
 async def sign_up_user(user: User):
